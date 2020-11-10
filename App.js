cc18bf1d83869f4705dbe020297200ddb6f94602
Guilherme <guilherme.schmitt@alkasoft.com.br>
2020-11-10 15:36:17 -0300

    Adicionado fonts customizadas para a aplicação, dependencias de roteamento e rotas iniciais

--- App.js
+++ App.js
@@ -1,109 +1,19 @@
 import React from 'react';
-import { Feather } from '@expo/vector-icons';
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
-import mapMarker from './src/images/map-marker.png';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: -27.5201881,
-          longitude: -48.6489143,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008
-        }}
-      >
-        <Marker
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.8,
-            y: 0.8
-          }}
-          coordinate={{
-            latitude: -27.5201881,
-            longitude: -48.6489143,
-          }}
-        >
-          <Callout
-            tooltip
-            onPress={() => alert('Oi')}
-          >
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>
-                Nome do orfanato
-              </Text>
-            </View>
-          </Callout>
-        </Marker>
-      </MapView>
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  });
 
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>
-          Um orfanato encontrado
-        </Text>
-        <TouchableOpacity
-          onPress={() => { }}
-          style={styles.createOrphanageButton}
-        >
-          <Feather
-            size={20}
-            name='plus'
-            color='#fff'
-          />
-        </TouchableOpacity>
-      </View>
-    </View>
+  if (!fontsLoaded)
+    return null;
+  return (
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  },
-  calloutContainer: {
-    width: 160,
-    height: 46,
-    borderRadius: 16,
-    paddingHorizontal: 16,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(255,255,255,0.8)',
-  },
-  calloutText: {
-    fontSize: 14,
-    color: '#0089a5',
-  },
-  footer: {
-    left: 24,
-    right: 24,
-    bottom: 32,
-    height: 56,
-    elevation: 3,
-    paddingLeft: 24,
-    borderRadius: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'absolute',
-    backgroundColor: '#fff',
-    justifyContent: 'space-between',
-  },
-  footerText: {
-    color: '#8fa7b3'
-  },
-  createOrphanageButton: {
-    width: 56,
-    height: 56,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#15c3d6',
-  }
-});
---
+++ src/pages/OrphanageDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function OrphanageDetails() {
+  return (
+    <View>
+      <Text>
+        TESTE
+      </Text>
+    </View>
+  )
+}
+
+export default OrphanageDetails
